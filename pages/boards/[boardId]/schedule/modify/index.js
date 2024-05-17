6575c9e230e1dc76fa0e270f19cff386e52ea387
eunn43 <soc06241@naver.com>
2024-05-17 15:35:05 +0900

    feat: 일정 컴포넌트 추가

---
+++ pages/boards/[boardId]/schedule/modify/index.js
@@ -0,0 +1,214 @@
+import { useRouter } from "next/router";
+import { useEffect, useState, useRef } from "react";
+import { getSchedule, modSchedule } from "@/utils/scheduleStorage";
+import { getColors, modColors } from "@/utils/colorsStorage";
+import {
+  MemoInput,
+  MemoBox,
+  SchedulePage,
+  ScheduleWrapper,
+  ScheduleTimeBox,
+  ScheduleTime,
+  ScheduleContentBox,
+  ScheduleContentInput,
+} from "@/styles/detailSchedule";
+import { CardBackGround, BtnWrapper, Btn } from "@/styles/detail";
+import { Page } from "@/styles/new";
+import {
+  Color,
+  ColorBox,
+  ColorPickerBox,
+  ColorWrapper,
+} from "styles/detailSchedule";
+import { HexAlphaColorPicker } from "react-colorful";
+
+const start = 6;
+const end = 24;
+
+export default function ModifySchedulePage() {
+  const router = useRouter();
+  const [date, setDate] = useState(router.query.boardId);
+  const [schedule, setSchedule] = useState({
+    timetable: Array(end - start).fill({ task: "", color: "" }),
+    memo1: "",
+    memo2: "",
+  });
+  const [showPicker, setShowPicker] = useState(false);
+  const ColorPickerBoxRef = useRef(null);
+  const [colors, setColors] = useState(["", "", ""]);
+  const [selectedColorIdx, setSelectedColorIdx] = useState(-1);
+  const [color, setColor] = useState("");
+
+  useEffect(() => {
+    const onClickColorOutside = (e) => {
+      if (
+        ColorPickerBoxRef.current &&
+        !ColorPickerBoxRef.current.contains(e.target)
+      ) {
+        setShowPicker(false);
+      }
+    };
+
+    if (showPicker) document.addEventListener("click", onClickColorOutside);
+    else document.removeEventListener("click", onClickColorOutside);
+    return () => {
+      document.removeEventListener("click", onClickColorOutside);
+    };
+  }, [showPicker]);
+
+  useEffect(() => {
+    if (selectedColorIdx !== -1) {
+      const newColors = [...colors];
+      newColors[selectedColorIdx] = color;
+      setColors(newColors);
+      modColors(newColors);
+    }
+  }, [color]);
+
+  useEffect(() => {
+    if (!router.query.boardId) return;
+    setDate(router.query.boardId);
+    setColors(getColors());
+  }, [router.query.boardId]);
+
+  useEffect(() => {
+    if (!date) return;
+
+    const scheduleData = getSchedule(date);
+
+    if (scheduleData) {
+      setSchedule({
+        timetable: [...scheduleData.timetable],
+        memo1: scheduleData.memo1,
+        memo2: scheduleData.memo2,
+      });
+    }
+  }, [date]);
+
+  const handleDragStart = (e, idx) => {
+    e.dataTransfer.setData("index", idx);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e, dropIdx) => {
+    e.preventDefault();
+    const dragIdx = parseInt(e.dataTransfer.getData("index"));
+    updateTimetableColor(dragIdx, dropIdx);
+  };
+
+  const updateTimetableColor = (dragIdx, dropIdx) => {
+    const newTimetable = schedule.timetable.map((item, idx) => {
+      if (idx >= dragIdx && idx <= dropIdx) return { ...item, color: color };
+      return item;
+    });
+
+    setSchedule((prevSchedule) => ({
+      ...prevSchedule,
+      timetable: newTimetable,
+    }));
+  };
+
+  const onScheduleKeyDown = (e, idx) => {
+    if (e.key === "Enter" || e.key === "ArrowDown") {
+      e.preventDefault();
+
+      const nextInput = document.getElementById(`scheduleInput${idx + 1}`);
+      if (nextInput) nextInput.focus();
+    }
+    if (e.key === "ArrowUp") {
+      e.preventDefault();
+
+      const nextInput = document.getElementById(`scheduleInput${idx - 1}`);
+      if (nextInput) nextInput.focus();
+    }
+  };
+
+  const onClickCancelColor = () => {
+    setSelectedColorIdx(-1);
+    setColor("");
+  };
+
+  const onClickColor = (idx) => {
+    if (selectedColorIdx !== idx) setSelectedColorIdx(idx);
+    setShowPicker(true);
+    setColor(colors[idx]);
+  };
+
+  const onClickClose = () => {
+    router.push(`/boards/${date}`);
+  };
+
+  const onClickSubmit = () => {
+    modSchedule(date, schedule.timetable, schedule.memo1, schedule.memo2);
+    router.push(`/boards/${date}`);
+  };
+
+  return (
+    <Page>
+      <SchedulePage>
+        <CardBackGround>
+          <ScheduleWrapper>
+            <ScheduleTimeBox>
+              {schedule.timetable.map((item, idx) => (
+                <ScheduleTime
+                  schedule={item.color}
+                  draggable="true"
+                  onDragStart={(e) => handleDragStart(e, idx)}
+                  onDragOver={handleDragOver}
+                  onDrop={(e) => handleDrop(e, idx)}
+                >
+                  {(idx + start).toString().padStart(2, "0")}
+                </ScheduleTime>
+              ))}
+            </ScheduleTimeBox>
+            <ScheduleContentBox>
+              {schedule.timetable.map((item, idx) => (
+                <ScheduleContentInput
+                  id={`scheduleInput${idx}`}
+                  type="text"
+                  placeholder={item.task}
+                  maxlength={14}
+                  onKeyDown={(e) => onScheduleKeyDown(e, idx)}
+                />
+              ))}
+            </ScheduleContentBox>
+            <MemoBox>
+              <BtnWrapper>
+                <Btn onClick={onClickSubmit}>등록</Btn>
+                <Btn onClick={onClickClose}>닫기</Btn>
+              </BtnWrapper>
+              <ColorWrapper>
+                <ColorBox>
+                  <Color
+                    isSelected={selectedColorIdx === -1}
+                    onClick={onClickCancelColor}
+                  ></Color>
+                  {colors.map((color, idx) => (
+                    <Color
+                      color={color}
+                      isSelected={selectedColorIdx === idx}
+                      onClick={() => onClickColor(idx)}
+                    />
+                  ))}
+                </ColorBox>
+                {showPicker && (
+                  <ColorPickerBox ref={ColorPickerBoxRef}>
+                    <HexAlphaColorPicker
+                      color={color}
+                      onChange={setColor}
+                    ></HexAlphaColorPicker>
+                  </ColorPickerBox>
+                )}
+              </ColorWrapper>
+              <MemoInput>{schedule.memo1}</MemoInput>
+              <MemoInput>{schedule.memo2}</MemoInput>
+            </MemoBox>
+          </ScheduleWrapper>
+        </CardBackGround>
+      </SchedulePage>
+    </Page>
+  );
+}
--- styles/detailSchedule.js
+++ styles/detailSchedule.js
@@ -1,5 +1,18 @@
 import styled from "@emotion/styled";
 
+export const SchedulePage = styled.div`
+  width: 500px;
+  height: 720px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  border: 1px solid lightgray;
+  border-radius: 16px;
+  box-shadow: 5px 5px 16px lightgray;
+`;
+
 export const ScheduleWrapper = styled.div`
   width: 100%;
   height: 100%;
@@ -22,7 +35,8 @@ export const ScheduleTime = styled.div`
   padding: 4px 0 0 2px;
   border-top: 0.5px solid var(--navy);
 
-  border-right: ${({ schedule }) => (schedule ? "10px solid skyblue" : "none")};
+  border-right: ${({ schedule }) =>
+    schedule ? "10px solid " + schedule : "none"};
   display: flex;
   align-items: start;
   justify-content: start;
@@ -40,6 +54,21 @@ export const ScheduleContent = styled.div`
   padding-left: 10px;
 `;
 
+export const ScheduleContentInput = styled.input`
+  width: 200px;
+  height: 35px;
+  border: none;
+  border-top: 0.5px solid var(--navy);
+  padding-left: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: start;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
 export const MemoBox = styled.div`
   width: 100%;
   height: 100%;
@@ -53,8 +82,57 @@ export const Memo = styled.div`
   width: 90%;
   height: 40%;
   border-top: 2.6px solid var(--mustard);
-  background-color: rgba(255, 219, 88, 0.5);
+  border-radius: 4px;
+  background-color: rgba(255, 219, 88, 0.2);
   padding: 10px;
   white-space: pre-wrap;
   line-height: 1.6;
+`;
+
+export const MemoInput = styled.textarea`
+  width: 90%;
+  height: 40%;
+  border: none;
+  border-top: 2.6px solid var(--mustard);
+  border-radius: 4px;
+  background-color: rgba(255, 219, 88, 0.2);
+  padding: 10px;
+  white-space: pre-wrap;
+  line-height: 1.6;
+  font-size: 16px;
+  resize: none;
+
+  &:focus {
+    outline: none;
+  }
+`;
+export const ColorWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`;
+export const ColorBox = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+`;
+
+export const Color = styled.div`
+  width: 34px;
+  height: 34px;
+  background-color: ${({ color }) => (color ? color : "#00408022")};
+  border-radius: 50%;
+  margin: 0 4px;
+  box-shadow: ${({ isSelected }) => (isSelected ? "inset 0 0 7px gray" : "")};
+`;
+
+export const ColorPickerBox = styled.div`
+  position: absolute;
+  top: 40px;
+  left: 0;
+  padding: 8px;
+  background-color: white;
+  border-radius: 8px;
 `;
